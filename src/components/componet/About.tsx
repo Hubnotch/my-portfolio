e901c9653c44784329b78eda07dbec9cfd4d34bc
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-03-10 14:42:57 +0100

    reusable header component

--- src/components/componet/About.tsx
+++ src/components/componet/About.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Heading } from '../util-component'
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className='p-12'>About</div>
+    <div className='w-full'>
+      <Heading title='About' />
+      <div className="flex">
+        <div className="">Write</div>
+        <div className="">Pic</div>
+      </div>
+    </div>
   )
 }
 export default About
