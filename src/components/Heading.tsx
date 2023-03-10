54405ffd200a092c212df94e04121deb44a6924e
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-03-10 07:51:37 +0100

    initial commit

---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// type Props = {}
+
+const Heading = (props:string) => {
+  return (
+    <h2 className='text-2xl'>{props}</h2>
+  )
+}
+
+export default Heading
---
+++ src/pages/hold.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const hold = (props: Props) => {
+  return (
+    <div>hold</div>
+  )
+}
+
+export default hold
