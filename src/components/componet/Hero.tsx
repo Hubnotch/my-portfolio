4402bc0c677c7851b89db82a26699aa8e89cd512
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-03-10 14:00:06 +0100

    removed outline on email anchor

--- src/components/componet/Hero.tsx
+++ src/components/componet/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <h2 className='my-7 font-light md:text-4xl max-w-2xl text-2xl'>
         I&apos;m <span className='font-bold'>Ekene Ugwuanyi</span>, a front-end software engineer focused on building beautiful interfaces & experiences. </h2>
       <h3 className='my-5 text-sm md:text-xl'>
-        <span>Get in touch </span><a className='py-1 border-b-2 border-sky-500' href="mailto:love2ekene@gmail.com" target='_blank'>love2ekene@gmil.com</a></h3>
+        <span>Get in touch </span><a className='py-1 border-b-2 border-sky-500 outline-none' href="mailto:love2ekene@gmail.com" target='_blank'>love2ekene@gmil.com</a></h3>
     </div>
   )
 }
