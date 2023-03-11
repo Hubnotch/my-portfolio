487533df270d41a5fa5a593af90b4d801756edbb
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-03-11 09:34:25 +0100

    about commit

--- src/components/componet/About.tsx
+++ src/components/componet/About.tsx
@@ -8,7 +8,16 @@ const About = (props: Props) => {
     <div className='w-full'>
       <Heading title='About' />
       <div className="flex">
-        <div className="">Write</div>
+        <div className="">
+          <p>Hello! My name is Ekene Ugwuanyi.I a front-end software engineer. I am passionate about creating dynamic and visually stunning websites and applications that not only look great but also provide an exceptional user experience.
+          </p>
+          <p>With a strong background in web development, I specialize in writing clean, maintainable code using the latest technologies such as HTML5, CSS3, and JavaScript frameworks like React and Angular.
+
+          </p>
+          <p>
+            I am constantly staying up to date with the latest industry trends and best practices, and enjoy collaborating with cross-functional teams to deliver high-quality products. Above all, I am committed to creating intuitive and seamless user interfaces that enhance the overall user experience and drive business success.
+          </p>
+        </div>
         <div className="">Pic</div>
       </div>
     </div>
